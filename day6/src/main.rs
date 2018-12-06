c587cddbda62c0910ff7549e72b4b944d12b91ab
Sam Gonshaw <sam@gonshaw.com>
2018-12-06 17:03:05 +0000

    Day 6 part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -3,9 +3,10 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
-const NUM_POINTS : usize = 2000; // Just needs to be bigger than all input lengths
+const NUM_POINTS : usize = 50; // Just needs to be bigger than all input lengths
 const GRID_SIZE : usize = 360; // Just needs to be bigger than all x/y coords max
 const MAX_DISTANCE :usize = (2 * GRID_SIZE) + 1;
+const THRESHOLD :usize = 10000;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -33,8 +34,9 @@ fn main() {
             "str" => args[2].split(",").collect::<Vec<&str>>(),
             _ => panic!("Not enough valid args")
         };
-    let map = find_map(&vec);
+    let map = find_danger_map(&vec);
     println!("{}",countneighbours(&map));
+    println!("{}",find_safe_region(&vec));
 }
 
 fn parse_line(line : &str) -> (usize, usize) {
@@ -44,9 +46,41 @@ fn parse_line(line : &str) -> (usize, usize) {
     (x, y)
 }
 
+fn find_safe_region(x: &Vec<&str>) -> u32 {
+    let mut map = vec![vec![0;GRID_SIZE];GRID_SIZE];
+    for line in x.iter().enumerate() {
+        let curpoint = parse_line(line.1);
+        if curpoint.0 >= GRID_SIZE || curpoint.1 >= GRID_SIZE {
+            panic!("Grid size not big enough");
+        }
+        println!("{}: {:?}", line.0, curpoint);
+        for xc in 0..GRID_SIZE {
+        for yc in 0..GRID_SIZE {
+            let xci: isize = xc as isize;
+            let yci: isize = yc as isize;
+            let cpxi: isize = curpoint.0 as isize;
+            let cpyi: isize = curpoint.1 as isize;
+            let mut distance = ((xci - cpxi).abs() + (yci - cpyi).abs()) as usize;
+            println!("({},{})={}  {}", xc,yc,map[xc][yc], distance);
+            map[xc][yc] += distance;
+        }
+        }
+    }
+    let mut out : u32 = 0;
+    for xc in 0..GRID_SIZE {
+    for yc in 0..GRID_SIZE {
+        if map[xc][yc] < THRESHOLD {
+            out+=1;
+            }
+    }
+    }
+    out
+}
+
+
 // TODO: fill in map from coords.
 //triangle ineq tells me d(x,y) <= |x| + |y|
-fn find_map(x: &Vec<&str>) -> Vec<Vec<usize>> {
+fn find_danger_map(x: &Vec<&str>) -> Vec<Vec<usize>> {
     // Initialise all as tied
     let mut map = vec![vec![(NUM_POINTS, MAX_DISTANCE);GRID_SIZE];GRID_SIZE];
     for line in x.iter().enumerate() {
