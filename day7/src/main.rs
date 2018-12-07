72069024e4c498b0aee2db2c61ccca9161c184b0
Sam Gonshaw <sam@gonshaw.com>
2018-12-07 18:37:02 +0000

    Day7 part 2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -6,6 +6,8 @@ use std::path::Path;
 
 const STAGES : usize = 26;
 const INVALID_STEP : usize = STAGES + 1;
+const WORKERS : usize = 5;
+const NORMAL_STAGE_TIME : usize = 60;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -33,8 +35,12 @@ fn main() {
             "str" => args[2].split(",").collect::<Vec<&str>>(),
             _ => panic!("Not enough valid args")
         };
-    let mut deps = parse_graph(&vec);
-    println!("{}",get_steps(&mut deps));
+    let deps = parse_graph(&vec);
+    let mut dept = deps.clone();
+    let mut deptz = deps.clone();
+    let order = get_steps(&mut dept);
+    println!("{}",&order);
+    println!("{}",get_time(&mut deptz));
 }
 
 fn parse_line(line : &str) -> (usize, usize) {
@@ -58,6 +64,7 @@ fn parse_graph(x : &Vec<&str>) -> Vec<Vec<usize>> {
         if !step.1 {
             deps[step.0].push(INVALID_STEP);
         }
+        else{ println!("{} REQ {:?}", step.0, deps[step.0]);}
     }
 
     deps
@@ -83,4 +90,52 @@ fn get_steps(x : &mut Vec<Vec<usize>>) -> String {
         else {break;}
     }
     out
+}
+
+fn get_time(x : &mut Vec<Vec<usize>>) -> usize {
+    let mut workers = [(INVALID_STEP,0);WORKERS];
+    let mut elapsed = 0;
+    let mut work_remaining = true;
+    while work_remaining {
+        // Tick on time and decrement work left for all workers
+        work_remaining = false;
+        for i in 0..WORKERS {
+            // If the worker is busy, decrease their time remaining
+            if workers[i].0 != INVALID_STEP {
+                workers[i].1 -= 1;
+                // if it finished
+                if workers[i].1 == 0 {
+                    // mark as done!
+                    for j in 0..STAGES {
+                        x[j].retain(|n| n != &workers[i].0);
+                    }
+                    workers[i].0 = INVALID_STEP;
+                }
+                else {
+                    work_remaining = true;
+                }
+            }
+		}
+		// look for workers without work and give them the next thing to do! 
+        for i in 0..WORKERS {
+            if workers[i].0 == INVALID_STEP {
+                // mark the step as assigned and give the worker the time to do it in
+                let mut next_char = INVALID_STEP;
+                for j in 0..STAGES {
+                    if x[j].len() == 0 {
+                        next_char = j;
+                        break;
+                    }
+                }
+                if next_char != INVALID_STEP  {
+                    println!("Assign {} to {} at time {}", next_char, i,elapsed);
+                    x[next_char].push(INVALID_STEP);
+                    workers[i] = (next_char, next_char + 1 + NORMAL_STAGE_TIME);
+                    work_remaining = true;
+                }
+            }
+        }
+        elapsed += 1;
+    }
+    elapsed - 1
 }
