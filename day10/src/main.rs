71bbef5e4994a11c2685c285136f979a58348c70
Sam Gonshaw <sam@gonshaw.com>
2018-12-10 17:32:49 +0000

    Day 10

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -3,8 +3,11 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
-const GRID_SIZE : isize = 20;
-const ATTEMPTS : usize = 5;
+const GRID_SIZE_X : isize = 250;
+const GRID_SIZE_Y : isize = 250;
+const BOUND : isize = 1000000;
+//const CHAIN : isize = 3;
+const ATTEMPTS : usize = 100000;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -44,24 +47,61 @@ fn parse_input(input : &Vec<&str>) {
         velocities.push((x,y));
         println!("{:?}",  (x,y));
     }
-    for _i in 0..ATTEMPTS {
+    for i in 0..ATTEMPTS {
+        //let mut sums = (0, 0);
+        //let mut sum  = (0, 0);
+        let mut min_x = BOUND;
+        let mut min_y = BOUND;
+        let mut max_x = -BOUND;
+        let mut max_y = -BOUND;
         for j in 0..points.iter().len() { 
             points[j].0 += velocities[j].0;
             points[j].1 += velocities[j].1;
+            if points[j].0 > max_x {
+                max_x = points[j].0;
+            }
+            if points[j].1 > max_y {
+                max_y = points[j].1;
+            }
+            if points[j].0 < min_x {
+                min_x = points[j].0;
+            }
+            if points[j].1 < min_y {
+                min_y = points[j].1;
+            }
+            //sum.0 += points[j].0;
+            //sum.1 += points[j].1;
+            //sums.0 += points[j].0 * points[j].0;
+            //sums.1 += points[j].1 * points[j].1;
         }
+        println!("{}: {} {} {} {}", i + 1, min_x, min_y, max_x, max_y);
+        if min_x >= 0 && min_y >=0  && max_x <  GRID_SIZE_X && max_y < GRID_SIZE_Y
+        {
+            //println!("xvar {} yvar {}", sums.0 - sum.0*sum.0, sums.1 - sum.1*sum.1);
         print_grid(&points);
+        }
     }
 }
 
 fn print_grid(x: &Vec<(isize,isize)>) {
     let mut out = String::new();
-    for i in -GRID_SIZE..GRID_SIZE {
-        for j in -GRID_SIZE..GRID_SIZE {
-            let point = (j as isize, i as isize);
-            out.push(
-            if x.contains(&point) {'#'} else {'.'});
+    //let mut valid = false;
+    for j in 0..GRID_SIZE_Y {
+        for i in 0..GRID_SIZE_X {
+            let point = (i as isize, j as isize);
+            let inside = x.contains(&point);
+      //      if !valid && inside && (j + CHAIN) < GRID_SIZE {
+      //          for k in 1..CHAIN {
+      //              let extra_point = (i as isize, k as isize);
+      //              valid = x.contains(&extra_point);
+      //              if !valid {break;};
+      //          }
+      //      }
+            out.push(if inside {'#'} else {'.'});
         }
         out.push('\n');
     }
-    println!("{}", out);
+    //if valid {
+        println!("{}", out);
+    //}
 }
